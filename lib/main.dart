5b602ab6e9387c8bfdf5b058acdebca6bcb6ff9b
vikash-kumar-sinha <vikashsinha4404@gmail.com>
2025-01-22 23:17:00 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -6,37 +6,31 @@ void main()=>runApp(MyApp());
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return MaterialApp(
-      home:ChangeBackground(),
-    );
+    return MaterialApp(home:ChangeBackground());
   }
-
 }
 
 class ChangeBackground extends StatefulWidget{
   @override
   State<ChangeBackground> createState()=>_ChangeBackgroundState();
+
 }
 
 class _ChangeBackgroundState extends State<ChangeBackground>{
   Color _backgroundColor=Colors.white;
-  var colorList=[Colors.white,Colors.green,Colors.red,Colors.blue,Colors.grey,Colors.orange];
 
   void _ChangeColor(){
-    setState((){
-      _backgroundColor=colorList[Random().nextInt(colorList.length-1)].withOpacity(1.0);
-    });
+    _backgroundColor=Color(Random().nextInt(0xFFFFFF)).withOpacity(1.0);
+    setState((){});
   }
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      backgroundColor: _backgroundColor,
-      body:Center(
-        child: ElevatedButton(
-          onPressed: _ChangeColor,
-          child: Text('Change Color'),
-        ),
-      ),
+      backgroundColor:_backgroundColor,
+      body:Center(child:ElevatedButton(
+        onPressed:_ChangeColor,
+        child:Text('change Color')
+      ))
     );
   }
 }
